8b1ecfb92bcef66539892d484c950b1258399834
Jklaric <klaric9jakov@gmail.com>
2024-04-21 23:06:54 +0200

    set up basic infrastructure and some styling of the log in view

---
+++ lib/views/login.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF0F3FF),
+      appBar: AppBar(
+        backgroundColor: Color(0xff211951),
+        title: Text('Welcome, please log in'),
+        titleTextStyle: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Color(0xffF0F3FF),
+        )
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 300,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        SvgPicture.asset(
+                          'assets/user.svg',
+                          width: 50,
+                          height: 50,
+                        ),
+                        Container(
+                          width: 150,
+                          height: 50,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              fillColor: Color(0xff211951),
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),),
+
+                              labelText: 'Username',
+                              labelStyle: TextStyle(
+                                color: Color(0xffF0F3FF),
+                              ),
+                            ),
+                            style: TextStyle(
+                              color: Color(0xffF0F3FF),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Icon(Icons.lock),
+                        Container(
+                          width: 150,
+                          height: 50,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              fillColor: Color(0xff211951),
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),),
+
+                              labelText: 'Password',
+                              labelStyle: TextStyle(
+                                color: Color(0xffF0F3FF),
+                              ),
+                            ),
+                            style: TextStyle(
+                              color: Color(0xffF0F3FF),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        TextButton(
+                          onPressed: (){print('ass');},
+                          child: Text(
+                            'Log In',
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Color(0xff15F5BA),
+                              decoration: TextDecoration.underline,
+                              decorationColor: Color(0xff15F5BA)
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: (){print('test');},
+                          child: Text(
+                            'New User?',
+                            style: TextStyle(
+                                fontSize: 13,
+                                color: Color(0xff211951),
+                                decoration: TextDecoration.underline,
+                                decorationColor: Color(0xff211951)
+                            ),
+                          ),
+                        ),],
+                    )]
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container( // This is the footer
+        color: Color(0xff211951),
+        height: 50.0,
+        alignment: Alignment.center,
+
+      ),
+    );
+  }
+}
