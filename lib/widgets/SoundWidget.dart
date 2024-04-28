76e1b5ab9b8a4f7dd473456af02f1396d70e5f32
Jklaric <klaric9jakov@gmail.com>
2024-04-28 17:44:49 +0200

    created sound level updating via API

--- lib/widgets/SoundWidget.dart
+++ lib/widgets/SoundWidget.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SoundWidget extends StatefulWidget {
@@ -10,6 +12,32 @@ class SoundWidget extends StatefulWidget {
 }
 
 class _SoundWidgetState extends State<SoundWidget> {
+
+  int retrievedSoundLevel = 0; // This will be updated with the actual sound level
+  late Timer _timer;//Timer to get the sound level every n amounts of time
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) => getSoundLevel());
+  } // This will get the sound level every second
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  } // This will cancel the timer when the widget is disposed
+
+
+  void getSoundLevel() async {
+    Response response = await get(Uri.parse('http://www.randomnumberapi.com/api/v1.0/random?min=0&max=99&count=1'));
+    List data = jsonDecode(response.body);
+    retrievedSoundLevel = data[0];
+    setState(() {});
+  } // This will get the sound level from the API
+
+
+
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
@@ -51,17 +79,20 @@ class _SoundWidgetState extends State<SoundWidget> {
                                 height: 40,
                               ),
                               Container(
+                                width: 70,
                                 padding: EdgeInsets.all(7),
                                 decoration: BoxDecoration(
                                   color: Color(0xff615B85),
                                   borderRadius: BorderRadius.circular(10),
                                 ),
-                                child: Text(
-                                  '70 dB',
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: Color(0xffF0F3FF),
+                                child: Center(
+                                  child: Text(
+                                    '$retrievedSoundLevel dB', // This will display the sound level
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xffF0F3FF),
+                                    ),
                                   ),
                                 ),
                               ),
