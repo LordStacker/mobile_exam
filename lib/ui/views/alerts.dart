4574b1769998894aae1a01ff8d44226f0205b639
Jklaric <klaric9jakov@gmail.com>
2024-05-16 18:05:43 +0200

    separate bottom navbar into separate class

--- lib/ui/views/alerts.dart
+++ lib/ui/views/alerts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
+import '../widgets/BottomNavbar.dart';
 import '../widgets/NotificationWidget.dart';
 
 class Alerts extends StatelessWidget {
@@ -20,118 +21,68 @@ class Alerts extends StatelessWidget {
             fontSize: 20,
             fontWeight: FontWeight.bold,
             color: Color(0xffF0F3FF),
-          )
-      ),
+          )),
       body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: NotificationWidget(),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: NotificationWidget(),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: NotificationWidget(),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: NotificationWidget(),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: NotificationWidget(),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: NotificationWidget(),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: NotificationWidget(),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: NotificationWidget(),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: NotificationWidget(),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: NotificationWidget(),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: NotificationWidget(),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: NotificationWidget(),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: NotificationWidget(),
-              ),
-
-
-            ],
-          ),
-        )
-      ),
-      bottomNavigationBar: Container( // This is the footer
-          color: Color(0xff211951),
-          height: 63.0,
-          alignment: Alignment.center,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/measurements');
-                },
-                child: SvgPicture.asset(
-                  'assets/svgs/meter.svg',
-                  width: 30,
-                  height: 30,
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: Color(0xff15F5BA),
-                    width: 1,
-                  ),
-                ),
-                child: SvgPicture.asset(
-                  'assets/svgs/Bell.svg',
-                  width: 40,
-                  height: 40,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/settings');
-                },
-                child: SvgPicture.asset(
-                  'assets/svgs/Settings.svg',
-                  width: 30,
-                  height: 30,
-                ),
-              ),
-
-            ],
-          )
-
-      ),
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: NotificationWidget(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: NotificationWidget(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: NotificationWidget(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: NotificationWidget(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: NotificationWidget(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: NotificationWidget(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: NotificationWidget(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: NotificationWidget(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: NotificationWidget(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: NotificationWidget(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: NotificationWidget(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: NotificationWidget(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 10),
+              child: NotificationWidget(),
+            ),
+          ],
+        ),
+      )),
+      bottomNavigationBar: const AppBottomNavigationBar(),
     );
   }
 }
--- lib/ui/views/measurements.dart
+++ lib/ui/views/measurements.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../bloc/measurements/measurements_bloc.dart';
+import '../widgets/BottomNavbar.dart';
 import '../widgets/HumidityWidget.dart';
 import '../widgets/SoundWidget.dart';
 import '../widgets/TemperatureWidget.dart';
@@ -89,62 +89,6 @@ class _MeasurementsState extends State<Measurements> {
             };
           },
         ),
-      ),
-    );
-  }
-}
-
-class AppBottomNavigationBar extends StatelessWidget {
-  const AppBottomNavigationBar({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      // This is the footer
-      color: Color(0xff211951),
-      height: 63.0,
-      alignment: Alignment.center,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(3),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                color: Color(0xff15F5BA),
-                width: 1,
-              ),
-            ),
-            child: SvgPicture.asset(
-              'assets/svgs/meter.svg',
-              width: 30,
-              height: 30,
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pushReplacementNamed(context, '/alerts');
-            },
-            child: SvgPicture.asset(
-              'assets/svgs/Bell.svg',
-              width: 40,
-              height: 40,
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pushReplacementNamed(context, '/settings');
-            },
-            child: SvgPicture.asset(
-              'assets/svgs/Settings.svg',
-              width: 30,
-              height: 30,
-            ),
-          ),
-        ],
       ),
     );
   }
